33e519db08d12508e723f4c059f6ea5ab4e32482
simone <simone.l.silva@parceiros.nos.pt>
2025-04-03 13:31:07 +0100

    chore: update variable enviroment

--- src/app/sobre-nos/page.js
+++ src/app/sobre-nos/page.js
@@ -4,7 +4,7 @@ export const metadata  = {
 }
 
 export default async function Page() {
-    const api = await fetch(`${process.env.BACKEND_URL}/api/about?populate=*`);
+    const api = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/about?populate=*`);
     const { data } = await api.json();
     const about = data;
 
